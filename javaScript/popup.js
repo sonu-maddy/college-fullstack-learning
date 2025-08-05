bcbe53d2e3cc24a09151f804ab01ab1af7d3fc5c
Sonu Maddheshiya <96307808+sonu-maddy@users.noreply.github.com>
2025-08-05 11:11:36 +0530

    new topic learn

---
+++ javaScript/popup.js
@@ -0,0 +1,10 @@
+// const button = document.getElementById("button")
+
+function showPop(){
+    document.getElementById("mymodel").style.display = "block";
+    document.getElementById("btn").style.display = "none"; 
+}
+function showbtn(){
+    document.getElementById("mymodel").style.display = "none"; 
+    document.getElementById("btn").style.display = "block";
+}
